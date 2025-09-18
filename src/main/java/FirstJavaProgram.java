c9787b70b175a97da43bea516501b35322949e30
Mir Monoarul Alam <anik_cse@live.com>
2025-09-18 09:14:31 +0600

    Initail Commit

---
+++ src/main/java/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program in my SQA Automation Journey!");
+    }
+}
