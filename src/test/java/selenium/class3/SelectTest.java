da85bd4aa3ee402999712b7e76f92b458979960f
Mir Monoarul Alam <anik_cse@live.com>
2025-10-13 08:47:39 +0600

    Added Country-State-City dropdown selection assignment

--- src/test/java/selenium/class3/SelectTest.java
+++ src/test/java/selenium/class3/SelectTest.java
@@ -1,10 +1,11 @@
 package selenium.class3;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SelectTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php/");
     }
 }
